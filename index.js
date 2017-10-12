261c077e179ea5232bcd3b8eb394d0c38b799028
lgants <lgants@gmail.com>
2017-10-12 18:53:28 -0500

    completed app

--- index.js
+++ index.js
@@ -52,17 +52,41 @@ class TopItems extends React.Component {
     let items = this.props.store.topStories.map(
       (store, idx) => <Item store={store} key={idx} />
     );
+
+    let variables = this.props.relay.variables;
+
+    // To reduce the perceived lag
+    // There are less crude ways of doing this, but this works for now
+    let currentStoryType = (this.state && this.state.storyType) || variables.storyType;
+
+    _onChange(ev) {
+      let storyType = ev.target.value;
+      this.setState({ storyType });
+      this.props.relay.setVariables({
+        storyType
+      });
+    }
+
     return <div>
+      <select onChange={this._onChange.bind(this)} value={currentStoryType}>
+        <option value="top">Top</option>
+        <option value="new">New</option>
+        <option value="ask">Ask HN</option>
+        <option value="show">Show HN</option>
+      </select>
       { items }
     </div>;
   }
 }
 
 TopItems = Relay.createContainer(TopItems, {
+  initialVariables: {
+    storyType: "top"
+  },
   fragments: {
     store: () => Relay.QL`
       fragment on HackerNewsAPI {
-        topStories { ${Item.getFragment('store')} },
+        stories(storyType: $storyType) { ${Item.getFragment('store')} },
       }
     `,
   },
